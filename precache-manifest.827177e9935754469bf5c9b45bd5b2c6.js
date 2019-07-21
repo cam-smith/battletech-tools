16c4b39c05813c47f590a72539d39b51e5f8a20c
Jeffrey D. Gordon <jeff@jdgwf.com>
2019-07-20 21:40:55 -0500

    Updates

---
+++ precache-manifest.827177e9935754469bf5c9b45bd5b2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b26fb328913b04fc16e84f7c7286bdd",
+    "url": "/battletech-tools/index.html"
+  },
+  {
+    "revision": "aec9557b1daa8f05597b",
+    "url": "/battletech-tools/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5bae7358ed5bfd9285a0",
+    "url": "/battletech-tools/static/css/main.f459f5df.chunk.css"
+  },
+  {
+    "revision": "aec9557b1daa8f05597b",
+    "url": "/battletech-tools/static/js/2.895e6b92.chunk.js"
+  },
+  {
+    "revision": "5bae7358ed5bfd9285a0",
+    "url": "/battletech-tools/static/js/main.bc9a77d4.chunk.js"
+  },
+  {
+    "revision": "ea3efc62bdcb3981b3fd",
+    "url": "/battletech-tools/static/js/runtime~main.2b1f72c3.js"
+  }
+]);
