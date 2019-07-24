e63bcaf54f5cb8211268b6497bb0bc10bf30b23f
Jeffrey D. Gordon <jeff@jdgwf.com>
2019-07-24 14:55:06 -0500

    Updates

--- precache-manifest.00a21e0f2147267263a422d3753269e6.js
+++ precache-manifest.00a21e0f2147267263a422d3753269e6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45da43814a4b255f68192db91757ddbf",
+    "revision": "12ef949c5fc9388958d25bd0d1dd5c87",
     "url": "/battletech-tools/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/battletech-tools/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "796a611b00893b6c66ce",
-    "url": "/battletech-tools/static/css/main.c9692ed4.chunk.css"
+    "revision": "0670aadcd805351809ba",
+    "url": "/battletech-tools/static/css/main.24183532.chunk.css"
   },
   {
     "revision": "05e6b307c1e8a9d9a4f1",
     "url": "/battletech-tools/static/js/2.3cf9dbba.chunk.js"
   },
   {
-    "revision": "796a611b00893b6c66ce",
-    "url": "/battletech-tools/static/js/main.5c33333c.chunk.js"
+    "revision": "0670aadcd805351809ba",
+    "url": "/battletech-tools/static/js/main.d864738c.chunk.js"
   },
   {
     "revision": "ea3efc62bdcb3981b3fd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/battletech-tools/precache-manifest.dad6fb50a8d29922235e1220063bc9b4.js"
+  "/battletech-tools/precache-manifest.00a21e0f2147267263a422d3753269e6.js"
 );
 
 self.addEventListener('message', (event) => {
